54444e41873d7b9f00d7adc5547e84d9e761f80c
Anna Sycheva <anna.sycheva@cleverpumpkin.ru>
2022-10-09 19:03:59 +0300

    homework 16

---
+++ Store/Store/CartViewController/CartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CartViewController.swift
+//  Store
+//
+//  Created by Анна Сычева on 08.10.2022.
+//
+
+import UIKit
+
+/// Экран корзины
+final class CartViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Store/Store/SearchViewController/Model/Product.swift
@@ -0,0 +1,12 @@
+//
+//  Product.swift
+//  Store
+//
+//  Created by Анна Сычева on 09.10.2022.
+//
+
+/// Модель описание продукта
+struct Product {
+    let name: String
+    let imageName: String
+}
